4a014b874401b7b92a255cfa7d42bef72ece1aae
sideman05 <eligobagobby@gmail.com>
2026-06-02 18:42:04 +0300

    workers

--- django_backend/api_app/management/commands/run_monitor_worker.py
+++ django_backend/api_app/management/commands/run_monitor_worker.py
@@ -20,8 +20,19 @@ class Command(BaseCommand):
         signal.signal(signal.SIGTERM, _stop_worker)
         signal.signal(signal.SIGINT, _stop_worker)
 
+        views._set_monitor_state(
+            running=True,
+            started_at=views._local_iso(),
+            last_event='Monitor worker booting',
+            event_count=0,
+        )
+
         self.stdout.write(self.style.SUCCESS('Monitor worker started'))
         try:
             views._monitor_runner(work_queue, stop_event)
+        except Exception as exc:
+            views._set_monitor_state(running=False, last_event=f'Monitor worker crashed: {exc}')
+            raise
         finally:
+            views._set_monitor_state(running=False, last_event='Monitor worker stopped')
             self.stdout.write(self.style.WARNING('Monitor worker stopped'))
